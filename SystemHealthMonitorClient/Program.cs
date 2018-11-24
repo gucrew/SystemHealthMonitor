1950c63d6d3050f749c0b39a8b85ff67778e7883
Wipoo <ais@live.com>
2018-11-24 19:24:23 +0700

    Start

---
+++ SystemHealthMonitorClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemHealthMonitorClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
