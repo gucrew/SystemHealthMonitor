323ad5236b0a1ba8ff746491c3d8867972a142e6
th62005274 <th62005274@CTHN0763>
2018-11-28 09:19:30 +0700

    Fix NuGet references

--- SystemHealthMonitorClient/Monitors.cs
+++ SystemHealthMonitorClient/Monitors.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ServiceProcess;
 using Microsoft.Web.Administration;
 using System.Data.Sql;
--- SystemHealthMonitorClient/Program.cs
+++ SystemHealthMonitorClient/Program.cs
@@ -30,18 +30,17 @@ namespace SystemHealthMonitorClient
                     list.AddRange(MSSQLMonitor.Run());
                     var report = new MonitorReport(list);
                     var json = JsonConvert.SerializeObject(report);
-                    var response = client.UploadString(ConfigurationManager.AppSettings["ReportUrl"], "post", json);
-
-                    File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt"), response);
+                    File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt"), json);
+                    client.UploadString(ConfigurationManager.AppSettings["ReportUrl"], "post", json);
                 }
                 catch (Exception e)
                 {
-                    File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt"), e.Message);
+                    //File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt"), e.Message);
                 }
 
                 File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "status.txt"), "Idle");
 
-                Thread.Sleep(1000 * 60 * 15);
+                Thread.Sleep(1000 * 60 * 60);
             }
         }
     }
