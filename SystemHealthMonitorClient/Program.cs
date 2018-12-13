0649fc0e710e8ed5ab1c6c841bb75b4197ccdc70
Wipoo <ais@live.com>
2018-12-13 09:41:03 +0700

    bug fixes

--- SystemHealthMonitorClient/Program.cs
+++ SystemHealthMonitorClient/Program.cs
@@ -14,23 +14,25 @@ namespace SystemHealthMonitorClient
         static void Main(string[] args)
         {
             File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "status.txt"), "Start");
-            var client = new WebClient { Encoding = Encoding.UTF8 };
-            client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
 
             while (true)
             {
                 try
                 {
-                    File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "status.txt"), "Working");
-                    var list = new List<MonitorInformation>();
-                    list.AddRange(IISServerMonitor.Run());
-                    list.AddRange(WindowsServiceMonitor.Run());
-                    list.AddRange(ProcessMonitor.Run());
-                    list.AddRange(MSSQLMonitor.Run());
-                    var report = new MonitorReport(list);
-                    var json = JsonConvert.SerializeObject(report);
-                    File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt"), json);
-                    client.UploadString(ConfigurationManager.AppSettings["ReportUrl"], "post", json);
+                    using (var client = new WebClient { Encoding = Encoding.UTF8 })
+                    {
+                        client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                        File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "status.txt"), "Working");
+                        var list = new List<MonitorInformation>();
+                        list.AddRange(IISServerMonitor.Run());
+                        list.AddRange(WindowsServiceMonitor.Run());
+                        list.AddRange(ProcessMonitor.Run());
+                        list.AddRange(MSSQLMonitor.Run());
+                        var report = new MonitorReport(list);
+                        var json = JsonConvert.SerializeObject(report);
+                        File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt"), json);
+                        client.UploadString(ConfigurationManager.AppSettings["ReportUrl"], "post", json);
+                    }
                 }
                 catch (Exception e)
                 {
